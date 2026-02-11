828f973200d4c9f67617f3130742d501a7b36486
joshuaaletile-byte <joshuaaletile@gmail.com>
2026-02-11 18:34:13 +0100

    Update and rename start.js to messages/start.js

---
+++ messages/start.js
@@ -0,0 +1,10 @@
+module.exports = `
+🔥 PH03NIX System Initialized
+
+Connection established successfully.
+Command interface is now available.
+
+Type /help to view available operations.
+
+POWERED BY PH03NIX🔥
+`;
