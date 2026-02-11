2de3404f7bacaabfca4155dc0cd44de8d8cdbb04
joshuaaletile-byte <joshuaaletile@gmail.com>
2026-02-11 16:53:26 +0100

    Add bot commands for start, help, and stats

--- bot.js
+++ bot.js
@@ -1,1 +1,31 @@
+const { OWNER_ID } = require("./config");
+const startMessage = require("./messages/start");
+const helpMessage = require("./messages/help");
+const { isOwner } = require("./utils/permissions");
 
+module.exports = function (bot) {
+
+  bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, startMessage, {
+      parse_mode: "Markdown"
+    });
+  });
+
+  bot.onText(/\/help/, (msg) => {
+    bot.sendMessage(msg.chat.id, helpMessage, {
+      parse_mode: "Markdown"
+    });
+  });
+
+  bot.onText(/\/stats/, (msg) => {
+    if (!isOwner(msg.from.id)) {
+      return bot.sendMessage(msg.chat.id, "⛔ Access denied.");
+    }
+
+    bot.sendMessage(
+      msg.chat.id,
+      "📊 System operational.\nAll core services are running.\n\nPOWERED BY PH03NIX🔥"
+    );
+  });
+
+};
