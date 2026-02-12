cc3bbbb24fc0d95f599e22275bd3bac2f6941700
joshuaaletile-byte <joshuaaletile@gmail.com>
2026-02-12 15:56:45 +0100

    Update adminhelp.js

--- commands/adminhelp.js
+++ commands/adminhelp.js
@@ -1,1 +1,16 @@
+module.exports = async (sock, msg) => {
+  const text = `
+🛠 ADMIN TOOLS
 
+/tagall — Notify everyone
+/poll — Start discussions
+/rules — Remind members
+/status — Check system
+
+Use responsibly to maintain order.
+
+POWERED BY PH03NIX🔥
+`;
+
+  await sock.sendMessage(msg.key.remoteJid, { text });
+};
