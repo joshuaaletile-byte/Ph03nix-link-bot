782048ebc0ecf26e5309980561c0b898ca621120
joshuaaletile-byte <joshuaaletile@gmail.com>
2026-02-12 15:47:29 +0100

    Update menu.js

--- commands/menu.js
+++ commands/menu.js
@@ -1,1 +1,21 @@
+module.exports = async (sock, msg) => {
+  const text = `
+⚡ PH03NIX COMMAND CENTER ⚡
 
+/menu — Show commands
+/motivate — Get motivation
+/joke — Random joke
+/time — Current time
+/rules — View group rules
+/poll — Create a poll
+/tagall — Call everyone
+/complaint — Submit complaint
+/status — Bot status
+/about — About system
+/adminhelp — Admin tools
+
+POWERED BY PH03NIX🔥
+`;
+
+  await sock.sendMessage(msg.key.remoteJid, { text });
+};
