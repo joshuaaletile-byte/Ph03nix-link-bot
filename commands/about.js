59b8b83c93e042a7f1e7d5213521be1cb9f0d56c
joshuaaletile-byte <joshuaaletile@gmail.com>
2026-02-12 15:55:47 +0100

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -1,1 +1,11 @@
+module.exports = async (sock, msg) => {
+  await sock.sendMessage(msg.key.remoteJid, {
+    text: `
+This automation node enhances communication,
+moderation and coordination across platforms.
 
+Engineered for reliable digital operations.
+
+POWERED BY PH03NIX🔥`
+  });
+};
