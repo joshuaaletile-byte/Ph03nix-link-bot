a982579b123d6216a80b82c18c5d7aa3f9bb518f
joshuaaletile-byte <joshuaaletile@gmail.com>
2026-02-12 15:51:09 +0100

    Update joke.js

--- commands/joke.js
+++ commands/joke.js
@@ -1,1 +1,10 @@
+const jokes = [
+  "Why did the phone go to school? To improve its connection.",
+  "My code works… I just don’t know why.",
+  "Debugging is like being a detective."
+];
 
+module.exports = async (sock, msg) => {
+  const joke = jokes[Math.floor(Math.random() * jokes.length)];
+  await sock.sendMessage(msg.key.remoteJid, { text: joke });
+};
