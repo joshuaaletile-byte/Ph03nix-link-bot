0d8a344239fc3e8814623cf34651c04c491f31a2
joshuaaletile-byte <joshuaaletile@gmail.com>
2026-02-12 15:54:23 +0100

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -1,1 +1,13 @@
+module.exports = async (sock, msg, text) => {
+  const question = text.replace("/poll", "").trim();
 
+  if (!question) {
+    return sock.sendMessage(msg.key.remoteJid, {
+      text: "Usage: /poll Your question here"
+    });
+  }
+
+  await sock.sendMessage(msg.key.remoteJid, {
+    text: `📊 POLL:\n${question}\n\nReply YES or NO`
+  });
+};
