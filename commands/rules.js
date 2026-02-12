4da4079324da2ca6e90e5be8c3bb706d3cec5c84
joshuaaletile-byte <joshuaaletile@gmail.com>
2026-02-12 15:52:52 +0100

    Update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -1,1 +1,14 @@
+module.exports = async (sock, msg) => {
+  const rules = `
+📜 GROUP RULES
 
+• No spam
+• Be respectful
+• No illegal content
+• Stay relevant
+
+POWERED BY PH03NIX🔥
+`;
+
+  await sock.sendMessage(msg.key.remoteJid, { text: rules });
+};
