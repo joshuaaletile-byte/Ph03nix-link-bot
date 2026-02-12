3be92560f01e226e4960db8c5e2cd4aa9e6364c6
joshuaaletile-byte <joshuaaletile@gmail.com>
2026-02-12 15:48:48 +0100

    Update complaint.js

--- commands/complaint.js
+++ commands/complaint.js
@@ -1,1 +1,8 @@
+module.exports = async (sock, msg) => {
+  await sock.sendMessage(msg.key.remoteJid, {
+    text: `Submit complaints here:
+https://joshuaaletile-byte.github.io/docs
 
+POWERED BY PH03NIX🔥`
+  });
+};
